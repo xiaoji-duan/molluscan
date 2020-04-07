231cd3e5d71e1121435605b95054e585fa5e650e
Leon <leon_xi@163.com>
2020-04-07 18:25:40 +0800

    查看git远程库信息

--- .travis/generate-before_script.sh
+++ .travis/generate-before_script.sh
@@ -7,4 +7,6 @@ echo "https://${U_NAME}:${GITHUB_TOKEN}@github.com" > ~/.git-credentials
 git config --global credential.helper store
 cat ~/.git-credentials
 
+git remote -v
+
 ionic login $IONIC_USERNAME $IONIC_PASSWORD
