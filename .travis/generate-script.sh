331012baf25749b039113129bb13b5453252ea84
Leon <leon_xi@163.com>
2020-04-07 20:20:59 +0800

    设置代码更新标志

--- .travis/generate-script.sh
+++ .travis/generate-script.sh
@@ -5,10 +5,11 @@ export UPDATED_FLAG="not updated"
 if [ ! -d "app" ]; then
   ionic start --appname molluscan --id com.xiaoji.molluscan app blank --capacitor
 
-  export UPDATED_FLAG="updated"
   # git add/commit
   git add app
   git commit -m "initial app commit"
+
+  export UPDATED_FLAG="updated"
 fi
 
 cd app
@@ -19,6 +20,8 @@ ionic cordova plugin add cordova-plugin-app-version cordova-sqlite-storage
 # git add/commit
 git add .
 git commit -m "update cordova plugin"
+
+export UPDATED_FLAG="updated"
 
 echo display molluscan files
 
