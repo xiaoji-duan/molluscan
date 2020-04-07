70b8a8a7849f56aa2c1425c1c1958b1909d8e422
Leon <leon_xi@163.com>
2020-04-07 17:40:28 +0800

    更新文件位置

--- .travis/generate-before_script.sh
+++ .travis/generate-before_script.sh
@@ -3,8 +3,8 @@
 git config user.name "${U_NAME}"
 git config user.email "${U_EMAIL}"
 
-echo "https://${U_NAME}:${GITHUB_TOKEN}@github.com" > .git-credentials
-git config --global credential.helper store --file=./.git-credentials
-cat .git-credentials
+echo "https://${U_NAME}:${GITHUB_TOKEN}@github.com" > ~/.git-credentials
+git config --global credential.helper store
+cat ~/.git-credentials
 
 ionic login $IONIC_USERNAME $IONIC_PASSWORD
