a510ce286750515e84f9108c0f353d381f193e8c
Leon <leon_xi@163.com>
2020-04-07 17:36:30 +0800

    更新git用户名和密码

--- .travis/generate-before_script.sh
+++ .travis/generate-before_script.sh
@@ -4,7 +4,7 @@ git config user.name "${U_NAME}"
 git config user.email "${U_EMAIL}"
 
 echo "https://${U_NAME}:${GITHUB_TOKEN}@github.com" > .git-credentials
-git config --global credential.helper store
+git config --global credential.helper store --file=./.git-credentials
 cat .git-credentials
 
 ionic login $IONIC_USERNAME $IONIC_PASSWORD
--- .travis/generate-script.sh
+++ .travis/generate-script.sh
@@ -6,6 +6,7 @@ if [ ! -d "app" ]; then
   # git add/commit/push
   git add app
   git commit -m "initial app commit"
+  # 提交 Github Pages
   #git push --force --quiet "https://${GITHUB_TOKEN}@${GITHUB_REF}" master:${P_BRANCH}
   git push origin master
 fi
