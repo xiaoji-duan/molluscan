84b82bb3fe235b63f037ad7eb9f40ade93620e51
Leon <leon_xi@163.com>
2020-04-07 16:15:07 +0800

    增加自动构建提交

--- .travis/generate-before_script.sh
+++ .travis/generate-before_script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -v
 
+git config user.name "${U_NAME}"
+git config user.email "${U_EMAIL}"
+
 ionic login $IONIC_USERNAME $IONIC_PASSWORD
--- .travis/generate-script.sh
+++ .travis/generate-script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -v
 
-ionic start --appname molluscan --id com.xiaoji.molluscan app blank
+if [ ! -d "app" ]; then
+  ionic start --appname molluscan --id com.xiaoji.molluscan app blank
+
+  # git add/commit/push
+  git add all
+  git commit -m "initial app commit"
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:${P_BRANCH}
+fi
 
 echo display molluscan files
 
