64a03128fe9318654b9125a48e6a87b441b16225
Leon <leon_xi@163.com>
2020-04-07 18:47:33 +0800

    git push https://[secure]@github.com/xiaoji-duan/molluscan HEAD:<name-of-remote-branch>

--- .travis/generate-script.sh
+++ .travis/generate-script.sh
@@ -8,7 +8,7 @@ if [ ! -d "app" ]; then
   git commit -m "initial app commit"
   # 提交 Github Pages
   #git push --force --quiet "https://${GITHUB_TOKEN}@${GITHUB_REF}" master:${P_BRANCH}
-  git push "https://${GITHUB_TOKEN}@${GITHUB_REF}"
+  git push "https://${GITHUB_TOKEN}@${GITHUB_REF}" HEAD:master
 fi
 
 echo display molluscan files
