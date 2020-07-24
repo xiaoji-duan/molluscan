6f7c75644fa5d26ebed2b8e10a22e16691301e78
席理加 <leon_xi@163.com>
2020-07-24 16:01:19 +0800

    increase-memory-limit

--- .travis/build-script.sh
+++ .travis/build-script.sh
@@ -5,4 +5,7 @@ cd app
 cordova platform remove android
 cordova platform add android@7.1.4
 
+npm install -g increase-memory-limit
+increase-memory-limit
+
 ionic cordova build android --prod --verbose --stacktrace
