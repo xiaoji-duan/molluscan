a6ef8344d0e805893a7f2ee6593615029c656fff
席理加 <leon_xi@163.com>
2020-07-25 11:55:02 +0800

    android licenses

--- .travis/build-install.sh
+++ .travis/build-install.sh
@@ -2,3 +2,7 @@
 
 gem install fir-cli
 yarn global add ionic phonegap plugman cordova
+
+mkdir -p $ANDROID_HOME/licenses
+echo -e "d56f5187479451eabf01fb78af6dfcb131a6481e" >> $ANDROID_HOME/licenses/android-sdk-license
+echo -e "84831b9409646a918e30573bab4c9c91346d8abd" >> $ANDROID_HOME/licenses/android-sdk-preview-license
