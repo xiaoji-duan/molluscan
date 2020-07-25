af47d0f8d7b36494b97cc7ee3e6b86923eeece28
席理加 <leon_xi@163.com>
2020-07-25 13:35:38 +0800

    export PATH

--- .travis/build-install.sh
+++ .travis/build-install.sh
@@ -3,8 +3,6 @@
 gem install fir-cli
 yarn global add @ionic/cli phonegap plugman cordova
 
-export PATH=$HOME/.yarn/bin:$PATH
-
 mkdir -p $ANDROID_HOME/licenses
 echo -e "d56f5187479451eabf01fb78af6dfcb131a6481e" >> $ANDROID_HOME/licenses/android-sdk-license
 echo -e "84831b9409646a918e30573bab4c9c91346d8abd" >> $ANDROID_HOME/licenses/android-sdk-preview-license
